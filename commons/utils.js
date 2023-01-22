8c01ab1e29ed54f477d2b128aa65cb18ccae7ed0
Dorian Souleyreau <doriansouleyreau@gmail.com>
2023-01-22 16:58:17 +0100

    Enhanced multi results characters count and too many results handling

--- commons/utils.js
+++ commons/utils.js
@@ -4,11 +4,47 @@ export function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+function stringEmbedLength(data) {
+    return JSON.stringify(data).length;
+}
+
+/**
+ * @param {import("discord.js").APIEmbed} data 
+ */
+function isValidEmbed(data) {
+    if ((data.title?.length ?? 0) > 256) return false;
+    if ((data.description?.length ?? 0) > 4096) return false;
+    if ((data.footer?.text?.length ?? 0) > 2048) return false;
+    if ((data.author?.name?.length ?? 0) > 256) return false;
+    if (Array.isArray(data.fields)) {
+        if (data.fields.length > 25) return false;
+        if (
+            data.fields.some(
+                field =>
+                    ((field.name?.length ?? 0) > 256)
+                    ||
+                    ((field.value.length ?? 0) > 1024)
+            )
+        ) return false;
+    }
+    if (embedLength(data) > 6000) return false;
+
+    return true;
+}
+
 export function replyWithEmbeds(embeds) {
     embeds = Array.isArray(embeds) ? embeds : [ embeds ];
+    if (stringEmbedLength(embeds) > 6500) {
+        return {
+            embeds: [{
+                title: 'Search: too many results',
+                description: 'Results contains too much lines to be displayed, please refine your search terms.'
+            }]
+        }
+    }
     const filteredEmbeds = [];
     for (const embed of embeds) {
-        if (embedLength(embed) < 6000) {
+        if (isValidEmbed(embed)) {
             filteredEmbeds.push(embed);
         }
         else {
@@ -24,14 +60,14 @@ export function replyWithEmbeds(embeds) {
                         null
 
             if (embed.title) {
-                console.trace(`Too long response${title ? ` for: ${title}` : ''}`);
+                console.log(`Too long response${title ? ` for: ${title}` : ''}`);
                 filteredEmbeds.push({
                     title: `${title ? `${title}: ` : ''}too long response`,
                     description: 'Please contact bot administrators.'
                 });
             }
             else {
-                console.trace(`Too many results${title ? ` for: ${title}` : ''}`);
+                console.log(`Too many results${title ? ` for: ${title}` : ''}`);
                 filteredEmbeds.push({
                     title: `${title ? `${title}: ` : ''}too many results`,
                     description: 'Results contains too much lines to be displayed, please refine your search terms.'
