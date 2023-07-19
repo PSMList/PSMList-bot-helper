54dfb7afc60ab4ecd6d890c800acfd06578cb204
Dorian Souleyreau <dorian@wikodit.fr>
2023-07-19 23:51:19 +0200

    BL-052-037 : Rework general display (list style, sentances order, some description...)

--- commons/help.js
+++ commons/help.js
@@ -1,36 +1,31 @@
-import { capitalize } from "./utils.js";
+import { prefix } from "../config.js";
 
-const indentation = '\u200b . \u200b ';
+const indentation = '\u200b \u200b \\> ';
+
+const slashHelp = ['ping', 'search id', 'search name', 'list', 'cost']
+    .reduce((help, command) => `${help}${indentation}\`/${command}\`\n`, '');
+
+const prefixHelp = ['ping', 'search', 'ship', 'fort', 'crew', 'treasure', 'keyword', 'list', 'cost']
+    .reduce((help, command) => `${help}${indentation}\`${prefix}${command}\`\n`, '');
 
 function allHelp(prefix) {
-	return prefix === '/' ?
-        `Available commands:
-		${
-			['ping', 'search id', 'search name', 'list', 'cost']
-				.reduce((help, command) => `${help}${indentation}\`${prefix}${command}\`\n`, '')
-		}
-		Type \`${prefix}help <command>\` to get detailed information.
+	return `Type \`${prefix}help <command>\` to get detailed information.
 
-		For further help, please check the [documentation](https://psmlist.com/public/blog/documentation_psmlisthelper) on psmlist.com.`
-        :
-        `Available commands:
-            ${
-                ['ping', 'search', 'ship', 'fort', 'crew', 'treasure', 'keyword', 'factions', 'expansions', 'rarities', 'cost']
-                    .reduce((help, command) => `${help}${indentation}\`${prefix}${command}\`\n`, '')
-            }
-            Type \`${prefix}help <command>\` or \`${prefix}<command> help\` to get detailed information.
-
-            For further help, please check the [documentation](https://psmlist.com/public/blog/documentation_psmlisthelper) on psmlist.com.`;
+        Available commands:
+		${prefix === '/' ? slashHelp : prefixHelp}
+		For further help, please check the [documentation](https://psmlist.com/public/blog/documentation_psmlisthelper) on psmlist.com.`;
 }
 
 export default function(command, prefix) {
     let helpMessage = '';
-    let helpTitle = `${capitalize(command)} help`;
+    let helpTitle = `Help for \`${command}\` command`;
+
+    if (command.startsWith('search')) {
+        command = 'search';
+    }
 
     switch (command) {
         case 'search':
-        case 'search id':
-        case 'search name':
             helpMessage =
                 `Type \`${prefix}search id <id>\` or \`${prefix}search name <name>\` to search in database\n` +
                 `Ex: \`${prefix}search id oe001\``
@@ -76,18 +71,13 @@ export default function(command, prefix) {
                 `Ex: \`${prefix}treasure id rof209\``
                 ;
             break;
-        case 'factions':
-            helpMessage = 'List of factions with their flag.';
-            break;
-        case 'expansions':
-        case 'extensions':
-            helpMessage = 'List of expansions with their flag, full name, short name, community short name and WizKids short name.';
-            break;
-        case 'rarities':
-            helpMessage = 'List of rarities with their color.';
-            break;
         case 'list':
-            helpMessage = 'Check all values of: expansions, factions, rarities, keyword categories or keyword targets.'
+            helpMessage = `Type \`/list <type>\`:
+            ${indentation}\`expansions\`: Official expansions
+            ${indentation}\`factions\`: Official factions for ships and crew
+            ${indentation}\`rarities\`: Rarities on cards
+            ${indentation}\`keyword categories\`: Keyword categories
+            ${indentation}\`keyword target\`: Possible targets for keywords`
             break;
         case 'cost':
             helpMessage = `Calculates the point value of a ship based on the [UDC](https://psmlist.com/public/udc_calculator) and [SimCost](https://psmlist.com/public/simcost_calculator) algorithms.
@@ -108,16 +98,18 @@ export default function(command, prefix) {
             helpTitle = 'Help';
             helpMessage = allHelp(prefix);
     }
-    if (['search', 'search id', 'search name', 'ship', 'fort', 'crew', 'treasure'].includes(command)) {
-        helpMessage += `\n\nType \`${prefix}${prefix === '/' ? 'search' : '<command>'} name "<text>"\` (double quote) to do an exact research.` +
-            '\n\nID research has a permissive syntax:\n' +
-            `${indentation}\`${prefix}expansions\` shows original, community and WizKids short names to use as a prefix\n` +
-            `${indentation}it is not case sensitive -> PotCC = potcc = POTCC\n` +
-            `${indentation}leading zeros are optional -> oe001 = oe01 = oe1`
+    if (['search', 'ship', 'fort', 'crew', 'treasure'].includes(command)) {
+        helpMessage += `
+                        Type \`${prefix}${command} name "<text>"\` (double quote) to do an exact research.
+
+                        ID research has a permissive syntax:
+                        ${indentation}\`${prefix}expansions\` shows original, community and WizKids short names to use as a prefix
+                        ${indentation}it is not case sensitive -> PotCC = potcc = POTCC
+                        ${indentation}leading zeros are optional -> oe001 = oe01 = oe1`
     }
 
     return {
         title: helpTitle,
-        description: helpMessage.replace(/^\s+/g, '')
+        description: helpMessage.replace(/^ +/gm, '')
     }
 }
